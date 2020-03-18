326bfc7f8e789952b34ed3ebe3375f5674ec9685
Mariana Meireles <mariana@psychonautgirl.space>
2020-03-18 23:00:04 +0100

    MacOS was a mistake

--- generic_game/DerivedData/generic_game/Build/Intermediates.noindex/generic_game.build/Debug/generic_game.build/Objects-normal/x86_64/ResourcePath.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/mariana/Development/dynamic-cast/game/generic_game/generic_game/ResourcePath.mm \
-  /Users/mariana/Development/dynamic-cast/game/generic_game/generic_game/ResourcePath.hpp
